48af2d3e97e552ed8770435577e7c443fcef246d
Morsalin3 <mdmorsalin3@gmail.com>
2022-12-29 12:20:32 +0600

    setup basic server

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000 ;
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ojgvcnk.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const PostsCollection = client.db("dbMedia").collection("posts");
+
+        app.get('/posts', async(req, res)=>{
+            const query = {};
+            const options = await PostsCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+app.get('/', async(req, res)=>{
+    res.send('media server is running');
+})
+
+app.listen(port, ()=>console.log(`Media is running on: ${port}`))
